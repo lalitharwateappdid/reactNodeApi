20d2b56d4e921678b4d44b355c42aaacdc61f59c
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-05-28 12:11:38 +0530

    literture model made commented for now

---
+++ models/LiteratureModel.js
@@ -0,0 +1,11 @@
+// const { DataTypes } = require("sequelize");
+
+// const sequelize = require("../database/database");
+
+// const Literature = sequelize.define("literatures",{
+//     "id":{
+//         type:DataTypes.INTEGER,
+//         primaryKey:true,
+//         autoIncrement:true
+//     },
+// })
