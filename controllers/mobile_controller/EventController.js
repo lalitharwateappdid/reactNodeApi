b0e367fe5807d42b3a636270721dfd6acb433b62
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-20 13:57:40 +0530

    updated

--- controllers/mobile_controller/EventController.js
+++ controllers/mobile_controller/EventController.js
@@ -1,9 +1,18 @@
 const db = require("../../database/database");
 const Event = require("../../models/EventModel");
+const { Op } = require('sequelize');
 
 exports.get = async (req, res) => {
     try {
-        const data = await Event.findAll()
+        const data = await Event.findAll({
+            where:{
+                status:true,
+                event_date:{
+                    [Op.gte]:Date.now()
+                }
+            },
+            order:[['event_date','ASC']]
+        })
 
         res.status(200).json({
             "data": data,
