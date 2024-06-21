8a4bfb3cb9c5024f149c6df20e0fdcd45f2c5b27
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-21 15:30:36 +0530

    updated

--- controllers/BusinessSettingsController.js
+++ controllers/BusinessSettingsController.js
@@ -2,8 +2,21 @@ const BusinessSettings = require("./../models/BusinessSettingModel")
 
 exports.create = async(req,res) => {
     try{
-        
+        const requestData = req.body;
+
+    // Iterate through each key-value pair in the request body
+    for (const key in requestData) {
+      if (requestData.hasOwnProperty(key)) {
+        const value = requestData[key];
+
+        // Update or create a BusinessSetting record based on 'key'
+        await BusinessSettings.upsert({
+          key: key,
+          value: value
+        });
+      }
     }
+}
     catch(err){
         res.status(400).json({
             "message":"Something went wrong"
