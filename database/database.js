2c6678361b44d3bc9a0feb775d2d409e4c6db45f
raiyan-appdid <117265374+raiyan-appdid@users.noreply.github.com>
2024-06-21 14:17:24 +0530

    updated

--- database/database.js
+++ database/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env.dbname,
     process.env.user, process.env.pasword, {
     host: process.env.host,
     dialect: "mysql",
-    port: process.env.port,
+    port: process.env.db_port,
 });
 
 
