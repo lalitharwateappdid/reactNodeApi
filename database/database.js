2ef0b4a1d251e793ba9f47affa2b3dd0e8863e1b
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-21 12:08:02 +0530

    updated

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.dbname,
-    process.env.user, null, {
+    process.env.user, process.env.pasword, {
     host: "localhost",
     dialect: "mysql"
 });
