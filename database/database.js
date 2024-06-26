6525ddc1d147bb972cad0db1487c180e97169bf2
raiyan-appdid <117265374+raiyan-appdid@users.noreply.github.com>
2024-06-26 18:35:31 +0530

    test

--- database/database.js
+++ database/database.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
+console.log('testing');
+console.log(process.env.user);
 const sequelize = new Sequelize(process.env.dbname,
     process.env.user, process.env.pasword, {
     host: process.env.host,
