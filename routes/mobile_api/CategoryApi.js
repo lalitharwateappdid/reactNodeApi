9b227f69f40dd63f3ebabd8dc4f9020261a63141
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-26 16:56:53 +0530

    updated

--- routes/mobile_api/CategoryApi.js
+++ routes/mobile_api/CategoryApi.js
@@ -1,7 +1,8 @@
 const express = require("express")
-const router = express.Router()
+const router = express.Router() 
 const Categories = require("../../controllers/mobile_controller/CategoriesController");
 
 router.get("/get", Categories.get)
 
 module.exports = router
+// updated
