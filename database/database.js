90aa39b33580799eb10e4051443e4295a952f2ff
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-26 17:04:35 +0530

    db updated

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.dbname,
-    process.env.user, process.env.pasword, {
+    "raiyan", process.env.pasword, {
     host: process.env.host,
     dialect: "mysql",
     port: process.env.db_port,
