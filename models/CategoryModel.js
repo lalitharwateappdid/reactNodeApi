a56526510de7844e664348814dee5a5acbd03c5d
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-28 21:00:21 +0530

    updated

--- models/CategoryModel.js
+++ models/CategoryModel.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database/database');
 
-const Category = sequelize.define('categories', {
+const Category = sequelize.define('Categories', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
