63c38a036ecda58f0fd265fa65e35035f4b65665
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-07-01 16:44:24 +0530

    updated

--- controllers/mobile_controller/CategoriesController.js
+++ controllers/mobile_controller/CategoriesController.js
@@ -57,14 +57,7 @@ exports.getByid = async (req, res) => {
             where: {
                 id: id
             },
-            include: [
-                {
-                    model: Category,
-                    as: 'relatedCategories', // Alias for the related categories
-                    through: { attributes: [] }, // Exclude attributes from join table
-                    attributes: ['id'], // Select only the id column from related categories
-                }
-            ]
+            
         });
 
         // Check if categories exist and fetch related categories recursively
